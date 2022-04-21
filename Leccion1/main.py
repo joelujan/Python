39530c4d76b64e4345adf750f68e47686d617be3
Joel Lujan <joelandreslujan@gmail.com>
2022-04-20 21:00:33 -0300

    Mi primer commit

---
+++ Leccion1/main.py
@@ -0,0 +1,34 @@
+miVariable = 3
+print(miVariable)
+miVariable = "abc def"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+# Las literales se escriben x088
+
+a: str = 10.78 #En este caso la variable sigue siendo float, y el "str" es solo para una referencia para uno mismo o para otro programador que lea nuestro código.
+print(type(a)) #Sirve para saber que tipo de variable es "a" si es string, int, float, etc.
+
+b = False #Variable tipo booleana (verdadero o falso). Se escribe con primer letra en Mayuscula.
+print(type(b))
+
+# Tipos int, float, String, Bool
+x = 10
+print(x)
+print(type(x))
+x = 14.5
+print(x)
+print(type(x))
+x = "Hola alumnos"
+print(x)
+print(type(x))
+x = True
+print(x)
+print(type(x))
+x = False
+print(x)
+print(type(x))
